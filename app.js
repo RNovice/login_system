2f930a5b5be6aa6220a59cc8af627e9c0f471dd8
RNovice <x38281146x@gmail.com>
2022-05-11 17:15:43 +0800

    add personal page & account check function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const exphbs = require('express-handlebars')
+const Acct = require('./user.json')
 
 const app = express()
 const port = 3000
@@ -7,8 +8,25 @@ const port = 3000
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true}))
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const foundData = Acct.users.find(user => user.email === req.body.email )
+  if (foundData){
+    if (req.body.password === foundData.password){
+      res.render('personal', { name: foundData.firstName})
+    } else {
+      res.render('index', { email: req.body.email, password: req.body.password, incorrect: 'incorrect email or password'})
+    }
+
+  } else {
+    res.render('index', { email: req.body.email, password: req.body.password, incorrect: 'incorrect email or password'})
+  }
+
 })
 
 app.listen(port, () => {
