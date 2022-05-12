8c7e4b4bf63c2dbffc182b8b3b2c91b7b10a56ff
RNovice <x38281146x@gmail.com>
2022-05-12 11:29:55 +0800

    add css style

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true}))
+app.use(express.static('pubilc'))
 
 app.get('/', (req, res) => {
   res.render('index')
